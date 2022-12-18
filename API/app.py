d738cf42044e027265164bebea8677096218a75b
Natez <lucas00013@gmail.com>
2022-12-18 01:04:18 -0300

    Update app.py

--- API/app.py
+++ API/app.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 CORS(app)
 def set_FirstData():
     # enter city name
-    city = "Santiago Chile"
+    city = "Santiago de Chile"
     
     # creating url and requests instance
     url = "https://www.google.cl/search?q="+"clima "+city
