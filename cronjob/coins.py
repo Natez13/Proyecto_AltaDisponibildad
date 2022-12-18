70894806baa23e73fd26a572258681a576d2756e
Natez <lucas00013@gmail.com>
2022-12-18 04:32:25 -0300

    Update coins.py

--- cronjob/coins.py
+++ cronjob/coins.py
@@ -6,12 +6,6 @@ from bs4 import BeautifulSoup
 import re
 import time
 
-number_pattern = "^\\d+$"
-re.match(number_pattern, '42') # Returns Match object
-re.match(number_pattern, 'notanumber') # Returns None
-# Extract number from a string
-number_extract_pattern = "\\d+"
-
 def main():
     with grpc.insecure_channel('backend:8050') as channel:
         stub = cron_pb2_grpc.DataStub(channel)
@@ -26,15 +20,18 @@ def main():
         html = requests.get(url).content
         soup = BeautifulSoup(html, 'html.parser')
         dolar_arr = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
-        dolar = re.findall(number_extract_pattern, dolar_arr)[0]
+        aux = dolar_arr.split(' ')
+        dolar = aux[0]
         html = requests.get(url2).content
         soup = BeautifulSoup(html, 'html.parser')
         euro_arr = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
-        euro = re.findall(number_extract_pattern, euro_arr)[0]
+        aux = euro_arr.split(' ')
+        euro = aux[0]
         html = requests.get(url3).content
         soup = BeautifulSoup(html, 'html.parser')
         uf_arr = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
-        uf = re.findall(number_extract_pattern, uf_arr)[0]
+        aux = uf_arr.split(' ')
+        uf = aux[0]
         epoch_time = str(int(time.time()))
         
         request = cron_pb2.GetCoinsDataRequest(dolar=dolar,euro=euro,uf=uf,time=epoch_time)
