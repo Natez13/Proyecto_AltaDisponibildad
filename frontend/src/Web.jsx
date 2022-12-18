93a9722874bf5d21b3a5766741f276feb662b2cd
Natez <lucas00013@gmail.com>
2022-12-18 01:33:57 -0300

    Update Web.jsx

--- frontend/src/Web.jsx
+++ frontend/src/Web.jsx
@@ -93,7 +93,7 @@ return (
         <div className="container">
           <div className="dashboard">
             <div className="dashboard-header">
-              <div className="dashboard-header__title">Chile Today 2</div>
+              <div className="dashboard-header__title">Chile Today 3</div>
               <div className="dashboard-header__date">{time?time:'0'}</div>
             </div>
             <div className="dashboard-card divisa">
